dc3043c938b09277faac7408b6722c1221d2476d
MysticalMike60t <cadenmfink@gmail.com>
2024-07-08 04:35:54 -0400

    Update generate-directory-index.js

--- generate-directory-index.js
+++ generate-directory-index.js
@@ -30,8 +30,8 @@ function generateIndex(dir) {
     body {background: rgb(25,25,25);display:flex;align-items:center;justify-content:flex-start;flex-direction:column;padding:20px 10px;}
     .title {font-size:2em;}
     .files {max-width:100%;min-width: 300px;background:rgb(35,35,35);border-radius:5px;box-shadow:0 0 5px 0 rgba(0,0,0,0.1);padding: 5px 20px;display:flex;align-items:center;justify-content:flex-start;flex-direction:column;}
-    ul {width:100%;margin:0;padding:0;list-style:none;display:flex;align-items:center;justify-content:flex-start;flex-direction:column;}
-    li {width:100%;height:auto;background:rgba(0,0,0,0.1);display:flex;align-items:center;justify-content:flex-start;flex-direction:row;padding:5px;padding-top:0;border-bottom:1px solid rgb(75,75,75);border-radius:5px;}
+    ul {width:100%;margin:0;padding:0;list-style:none;display:flex;align-items:center;justify-content:flex-start;flex-direction:column;gap:5px;}
+    li {width:100%;height:auto;background:rgba(0,0,0,0.1);display:flex;align-items:center;justify-content:flex-start;flex-direction:row;padding:5px;padding-top:0;border-bottom:1px solid var(--color-accent-hover);border-radius:5px;}
     li:hover {background:rgba(0,0,0,0.2);}
     li:last-child {border-bottom:none;}
     a {text-decoration:none;color:var(--color-accent);}
